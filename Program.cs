dd399924c29031c932273f8798f7616b02ae51dd
ShivamSaini490 <126521640+ShivamSaini490@users.noreply.github.com>
2023-12-04 14:42:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int numerator = 15;
+            int denominator = 0;
+
+            int result = DivideByZero(numerator, denominator);
+            Console.WriteLine("Result: " + result);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        Console.WriteLine("Program continues...");
+    }
+
+    static int DivideByZero(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            throw new DivideByZeroException("Denominator cannot be zero.");
+        }
+
+        return numerator / denominator;
+    }
+}
